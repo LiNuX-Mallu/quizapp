6d66311a1e3a83b1957fad3fcace58a05199f03c
LiNuX-Mallu <shaspachu457@gmail.com>
2024-03-02 19:47:52 +0530

    completed all fundamental features

--- client/src/pages/main/Main.tsx
+++ client/src/pages/main/Main.tsx
@@ -1,5 +1,4 @@
 import { useCallback, useContext, useEffect, useState } from "react";
-import axios from "../../instances/axios";
 import { ContextID } from "../../store/context";
 import { useNavigate } from "react-router-dom";
 import socket from "../../instances/socket";
@@ -15,110 +14,200 @@ interface Question {
 }
 
 export function Main() {
-  const [question, setQ] = useState<Question | null>(null);
-  const [preQ, setPreQ] = useState("");
+  const [questions, setQuestions] = useState<Question[] | null>(null);
+  const [qIndex, setQIndex] = useState(0);
   const { oppID, setOppID, ID } = useContext(ContextID);
   const navigate = useNavigate();
+  const [gameStarted, setGameStarted] = useState(false);
+  const [gameScore, setGameScore] = useState({ opp: 0, me: 0 });
 
-  const ask = () => {
-    axios
-      .post(
-        "/new-question",
-        { pre: preQ },
-        { headers: { "Content-Type": "application/json" } }
-      )
-      .then((res) => {
-        if (res.status === 200) {
-          setQ(res.data);
-          setPreQ(preQ + ", " + res.data.question);
-          console.log(res.data.question);
-        }
-      })
-      .catch(() => alert("Internal server error"));
-  };
+  const receiveExit = useCallback(
+    (id: string) => {
+      if (id !== oppID) return;
+      Swal.fire({
+        title: "You won!",
+        text: "Opponent left the game",
+        icon: "warning",
+        iconHtml: '<i class="fa-solid fa-trophy" style="color: #FFD43B;"></i>',
+      }).then(() => {
+        setOppID(null);
+        navigate("/");
+      });
+    },
+    [navigate, oppID, setOppID]
+  );
 
-  const receiveExit = useCallback((id: string) => {
-    if (id !== oppID) return;
-    Swal.fire({
-      title: 'You won!',
-      text: "Opponent left the game",
-      icon: 'warning',
-      iconHtml: '<i class="fa-solid fa-trophy" style="color: #FFD43B;"></i>',
-    })
-    .then(() => {
-      setOppID(null);
-      navigate('/');
-    })
-  }, [navigate, oppID, setOppID]);
+  function receiveQuestions(questions: Question[]) {
+    setQuestions(questions);
+  }
+
+  interface Game {
+    started: boolean;
+    score: number;
+    attempt: number;
+  }
+  const receiveScore = useCallback((game: Record<string, Game>) => {
+    if (!oppID || !ID) return;
+    setGameScore({opp: game[oppID].score, me: game[ID].score});
+    if (game[oppID].attempt === 5 || game[ID].attempt === 5) {
+      if (game[oppID].score > game[ID].score) {
+        Swal.fire({
+          title: "Opponent won the game",
+          text: `OPP: ${game[oppID].score} | YOU: ${game[ID].score}`,
+          icon: "warning",
+          iconHtml: '<i class="fa-solid fa-face-sad-tear"></i>',
+        }).then(() => {
+          setOppID(null);
+          navigate("/");
+        });
+      } else if (game[oppID].score < game[ID].score) {
+        Swal.fire({
+          title: "You won the game",
+          text: `OPP: ${game[oppID].score} | YOU: ${game[ID].score}`,
+          icon: "warning",
+          iconHtml: '<i class="fa-solid fa-trophy" style="color: #FFD43B;"></i>',
+        }).then(() => {
+          setOppID(null);
+          navigate("/");
+        });
+      } else {
+        Swal.fire({
+          title: "It's a draw",
+          text: `OPP: ${game[oppID].score} | YOU: ${game[ID].score}`,
+          icon: "warning",
+          iconHtml: '<i class="fa-solid fa-exclamation"></i>',
+        }).then(() => {
+          setOppID(null);
+          navigate("/");
+        });
+      }
+    }
+  }, [ID, oppID, setOppID, navigate])
 
   useEffect(() => {
-    socket.on('receiveExit', receiveExit);
+    if (oppID === null) return navigate("/");
+    socket.on("receiveExit", receiveExit);
+    socket.on("receiveQuestions", receiveQuestions);
+    socket.on('receiveScore', receiveScore);
 
     return () => {
-      socket.off('receiveExit', receiveExit);
+      socket.off("receiveExit", receiveExit);
+      socket.off("receiveQuestions", receiveQuestions);
+      socket.off('receiveScore', receiveScore);
+    };
+  }, [ID, oppID, receiveExit, navigate, receiveScore]);
+
+  const handleAnswer = (option: string, e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const target = e.target as HTMLElement;
+    const color = target.style.backgroundColor;
+    if (questions && option === questions[qIndex].answer) {
+      target.style.backgroundColor = "green";
+      socket.emit('sendScore', {scored: true, from: ID, to: oppID});
+    } else {
+      target.style.backgroundColor = "red";
+      socket.emit('sendScore', {scored: false, from: ID, to: oppID});
     }
-  }, [ID, oppID, receiveExit]);
+    if (qIndex < 4) {
+      setTimeout(() => {
+        setQIndex(qIndex + 1);
+        target.style.backgroundColor = color;
+      }, 1000);
+    }
+  };
 
   return (
-    <div className="select-none bg-gradient-to-br from-slate-400 to-slate-800 h-[100vh] flex justify-center items-between flex-col items-center overflow-hidden">
+    <div className="select-none bg-gradient-to-br from-slate-400 to-slate-800 h-[100vh] flex justify-between items-center flex-col overflow-hidden">
       <div className="bg-transparent mb-5 h-[10%] ps-10 pe-10 text-white w-[100%] flex flex-row justify-between items-center">
-        <button
-          onClick={ask}
-          className="flex items-center justify-center font-semibold"
-        >
+        <button className="flex items-center justify-center font-semibold">
           <i className="fa-solid fa-user"></i>&nbsp;{oppID ?? "Looking..."}
         </button>
         <button
           onClick={() => {
-            socket.emit('alertExit', {from: ID, to: oppID});
-            setOppID(null)
-            navigate('/')
+            socket.emit("alertExit", { from: ID, to: oppID });
+            setOppID(null);
+            navigate("/");
           }}
           className="flex items-center justify-center font-[Lexend]"
         >
           Exit &nbsp; <i className="fa-solid fa-right-from-bracket"></i>
         </button>
       </div>
-      <div className="bg-transparent h-[100%] w-[100%] md:h-[100vh] md:w-[50%] flex justify-evenly items-center flex-col pb-10">
-        <div className="flex flex-col w-[100%] h-[100%] justify-start items-center gap-5">
-          <div className="w-[50%] flex font-semibold text-gray-700 overflow-hidden rounded-md">
-            <div className="w-[100%] h-[100%] bg-blue-400 p-2 flex justify-center text-white">
-              YOU &nbsp; <span>0</span>
-            </div>
-            <div className="w-[100%] h-[100%] bg-red-400 p-2 flex justify-center text-white">
-              OPP &nbsp; <span>0</span>
+      {questions && (
+        <div className="bg-transparent h-[20%] w-[100%] md:h-[100vh] md:w-[50%] flex justify-evenly items-center flex-col pb-10">
+          <div className="flex flex-col w-[100%] h-[100%] justify-start items-center gap-5">
+            <div className="w-[50%] flex font-semibold text-gray-700 overflow-hidden rounded-md">
+              <div className="w-[100%] h-[100%] bg-blue-400 p-2 flex justify-center text-white">
+                YOU &nbsp; <span>{gameScore.me}</span>
+              </div>
+              <div className="w-[100%] h-[100%] bg-red-400 p-2 flex justify-center text-white">
+                OPP &nbsp; <span>{gameScore.opp}</span>
+              </div>
             </div>
           </div>
         </div>
+      )}
 
-        {/* <div className="h-[5rem] w-[5rem] bg-gray-500 rounded-[50%] flex justify-center items-center">
-          <span className="text-4xl">0</span>
-        </div> */}
+      {!questions && (
+        <div className="flex justify-center h-[100%] items-center flex-col text-center ">
+          <button
+            onClick={() => {
+              socket.emit("sendStart", { from: ID, to: oppID });
+              setGameStarted(true);
+            }}
+            className={`border p-1 pe-3 ps-3 text-blue-500 bg-white font-semibold rounded-md ${
+              !gameStarted && "hover:bg-blue-500 hover:text-white"
+            }`}
+          >
+            {gameStarted
+              ? "Waiting for opponent to start the game..."
+              : "Start Game"}
+          </button>
+          {gameStarted === false && (
+            <p className="p-5 font-[Lexend] text-sm text-gray-300">
+              Player who scores most out of 10 will be the winner. Time ends
+              when any player finishes answering 10 questions
+            </p>
+          )}
+        </div>
+      )}
 
-        <div className="w-[100%] flex flex-col h-[40%] justify-center items-center overflow-hidden">
-          <div className="bg-green-200 h-[60%] w-[70%] rounded-md flex justify-center items-center">
-            <p className="font-[Lexend] text-green-900 p-5 text-center">
-              {question?.question
-                ? question?.question
+      {questions && (
+        <div className="w-[100%] flex flex-col h-[100%] justify-end items-center overflow-hidden pb-5">
+          <div className="bg-green-200 h-fit w-[70%] rounded-md flex justify-center items-center">
+            <p className="font-[Lexend] text-sm text-green-900 p-5 text-center">
+              {questions[qIndex].question
+                ? questions[qIndex].question
                 : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"}
             </p>
           </div>
-          <div className="grid grid-cols-2 gap-1 mt-1 w-[70%] h-[40%] overflow-hidden font-semibold">
-            <div className="bg-slate-200 rounded-md h-full w-full flex justify-start items-center text-gray-900 hover:bg-yellow-200">
-              &nbsp; {`a) ${question?.a ?? "option a"}`}
+          <div className="grid grid-cols-2 gap-1 mt-1 w-[70%] overflow-hidden font-semibold">
+            <div
+              onClick={(e) => handleAnswer("a", e)}
+              className="bg-slate-200 rounded-md h-full w-full flex justify-start items-center text-gray-900 hover:bg-yellow-200"
+            >
+              &nbsp; {`a) ${questions[qIndex].a ?? "option a"}`}
             </div>
-            <div className="bg-slate-200 rounded-md h-full w-full flex justify-start items-center text-gray-900 hover:bg-yellow-200">
-              &nbsp; {`b) ${question?.b ?? "option b"}`}
+            <div
+              onClick={(e) => handleAnswer("b", e)}
+              className="bg-slate-200 rounded-md h-full w-full flex justify-start items-center text-gray-900 hover:bg-yellow-200"
+            >
+              &nbsp; {`b) ${questions[qIndex].b ?? "option b"}`}
             </div>
-            <div className="bg-slate-200 rounded-md h-full w-full flex justify-start items-center text-gray-900 hover:bg-yellow-200">
-              &nbsp; {`c) ${question?.c ?? "option c"}`}
+            <div
+              onClick={(e) => handleAnswer("c", e)}
+              className="bg-slate-200 rounded-md h-full w-full flex justify-start items-center text-gray-900 hover:bg-yellow-200"
+            >
+              &nbsp; {`c) ${questions[qIndex].c ?? "option c"}`}
             </div>
-            <div className="bg-slate-200 rounded-md h-full w-full flex justify-start items-center text-gray-900 hover:bg-yellow-200">
-              &nbsp; {`d) ${question?.d ?? "option d"}`}
+            <div
+              onClick={(e) => handleAnswer("d", e)}
+              className="bg-slate-200 rounded-md h-full w-full flex justify-start items-center text-gray-900 hover:bg-yellow-200"
+            >
+              &nbsp; {`d) ${questions[qIndex].d ?? "option d"}`}
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
--- server.ts
+++ server.ts
@@ -48,32 +48,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-//Open AI
-const ai = new Ai({
-  apiKey: OPENAI_API,
-});
-
-app.post("/api/new-question", async (req: Request, res: Response) => {
-  try {
-    const pre = req.body.pre ?? "";
-    const content = `Please provide a JSON stringfied quiz response with properties {question: question string, a: option a string, b: option b string, c: option c string, d: option d string, answer: option string} Make sure you only send the json string as response within the format i mentioned, because i will directly JSON parse it. Be interconnected; do not send duplicate response. do not send anything other than the quizz object. In addition make sure not to ask these questions [${pre}]`;
-    const chatCompletion = await ai.chat.completions.create({
-      messages: [{ role: "user", content }],
-      model: "gpt-3.5-turbo-0125",
-    });
-    if (chatCompletion?.choices[0]?.message?.content) {
-      res
-        .status(200)
-        .json(JSON.parse(chatCompletion.choices[0].message.content));
-    } else {
-      res.status(500).json({ message: "Internal server error" });
-    }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
-
 app.use(
   express.static(path.join(__dirname, "../client/dist"), { index: false })
 );
@@ -81,7 +55,6 @@ app.use(
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
-//END
 
 const server = http.createServer(app);
 
@@ -92,7 +65,35 @@ const io = new Server(server, {
   allowEIO3: true,
 });
 
+async function generateQuestions() {
+  const ai = new Ai({
+    apiKey: OPENAI_API,
+  });
+  try {
+    const pre: string[] = [];
+    const content = `Please provide a JSON stringfied array of 5 quiz response with properties {question: question string, a: option a string, b: option b string, c: option c string, d: option d string, answer: option string} Make sure you only send the json string as response within the format i mentioned, because i will directly JSON parse it. Be interconnected; do not send duplicate response. do not send anything other than the quizz object. In addition make sure not to ask these questions [${pre}], make sure it have 5 questions`;
+    const chatCompletion = await ai.chat.completions.create({
+      messages: [{ role: "user", content }],
+      model: "gpt-3.5-turbo-0125",
+    });
+    if (chatCompletion?.choices[0]?.message?.content) {
+      return JSON.parse(chatCompletion.choices[0].message.content);
+    } else return null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+interface Game {
+  started: boolean;
+  score: number;
+  attempt: number;
+}
+
+const game = <Record<string, Record<string, Game>>>{};
 io.on("connection", async (socket: Socket) => {
+  //join room
   socket.on("joinID", () => {
     let ID;
 
@@ -106,19 +107,21 @@ io.on("connection", async (socket: Socket) => {
     io.to(ID).emit("recieveID", ID);
   });
 
+  //leave room
   socket.on("leaveID", (ID: string) => {
     socket.leave(ID);
   });
 
-  socket.on("sendRequest", (data: {ID: string, friend: string}) => {
-	if (data.friend !== null) {
-		if (io.of("/").adapter.rooms.has(data.friend)) {
-			io.to(data.friend).emit('receiveRequest', data.ID);
-		} else {
-			io.to(data.ID).emit('receiveAlert', 404);
-		}
-		return;
-	}
+  //send request
+  socket.on("sendRequest", (data: { ID: string; friend: string }) => {
+    if (data.friend !== null) {
+      if (io.of("/").adapter.rooms.has(data.friend)) {
+        io.to(data.friend).emit("receiveRequest", data.ID);
+      } else {
+        io.to(data.ID).emit("receiveAlert", 404);
+      }
+      return;
+    }
     io.of("/").adapter.rooms.forEach((_, key) => {
       if (key !== data.ID && Number(key)) {
         io.to(key).emit("receiveRequest", data.ID);
@@ -126,16 +129,45 @@ io.on("connection", async (socket: Socket) => {
     });
   });
 
+  //accepting request
   socket.on("acceptRequest", (data: { from: string; to: string }) => {
-    if (io.of("/").adapter.rooms.has(data.from) && io.of("/").adapter.rooms.has(data.from)) {
-    	io.to(data.to).emit("receiveAccept", data.from);
-	}
-  });
-  socket.on('alertExit', (data: {from: string, to: string}) => {
-    if (data.from && data.to) {
-      io.to(data.to).emit('receiveExit', data.from);
+    if (
+      io.of("/").adapter.rooms.has(data.from) &&
+      io.of("/").adapter.rooms.has(data.from)
+    ) {
+      game[data.from] = {
+        [data.from]: { score: 0, attempt: 0, started: false },
+        [data.to]: { score: 0, attempt: 0, started: false },
+      };
+      game[data.to] = game[data.from];
+      io.to(data.to).emit("receiveAccept", data.from);
     }
-  })
+  });
+
+  //ready to start
+  socket.on("sendStart", (data: { from: string; to: string }) => {
+    game[data.from][data.from].started = true;
+    if (game[data.from][data.to].started === true) {
+      generateQuestions().then((res) => {
+        if (res !== null) {
+          io.to([data.to, data.from]).emit("receiveQuestions", res);
+        }
+      });
+    }
+  });
+
+  //score and attempt
+  socket.on("sendScore", (data: { scored: boolean; from: string; to: string }) => {
+    game[data.from][data.from].score += Number(data.scored);
+    game[data.from][data.from].attempt++;
+    io.to([data.from, data.to]).emit("receiveScore", game[data.from]);
+  });
+
+  socket.on("alertExit", (data: { from: string; to: string }) => {
+    if (data.from && data.to) {
+      io.to(data.to).emit("receiveExit", data.from);
+    }
+  });
 });
 
 //server running
